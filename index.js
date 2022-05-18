e3cc869c41b25933e964592d9c2ef80ffb7111b3
jikwan <jikwan0327@gmail.com>
2022-05-18 23:31:55 +0900

    todolist(3)

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-var toDoList = "";
-for (var i = 0; i < toDoList.length; i++) {
-    let doList = toDoList;
-    toDoList =
-        toDoList +
+var toDoList = Array();
+var i = 0;
+function getInput() {
+    toDoList.push(document.getElementById("addlist").value);
+    // "<li>" +
+    // "<button>X</button>" +
+    // '<input type="checkbox" id="checkList" />' +
+    // '<div id="newlist">' +
+    // "</div>" +
+    // "</li>";
+    document.querySelector("ul").innerHTML =
         "<li>" +
-        "<button>X</button>" +
-        '<input type="checkbox" id="checkList" />' +
+        '<button onclick="DeleteOne()">X</button>' +
+        '<input type="checkbox" id="checkList" onclick="is_checked()"/>' +
         '<div id="newlist">' +
-        "toDoList[i]" +
+        toDoList[i] +
         "</div>" +
         "</li>";
+    i++;
 }
-document.querySelector("ul").innerHTML = toDoList;
+// for (var i = 0; i < toDoList.length; i++) {
+//     toDoList[i] = toDoList;
+// }
 
-// const getInput = () => {
-// };
+const Delete = () => {
+    var j = toDoList.length;
+    toDoList.splice(0, j);
+    document.querySelector("ul").innerHTML = "";
+    i = 0;
+};
+function is_checked() {
+    var checkbox = document.getElementById("checkList");
+    var is_checked = checkbox.checked;
+    if (is_checked) {
+        document.getElementById("newlist").style.color = "rgba(200, 200, 200, 100";
+        document.getElementById("newlist").style.textDecoration = "line-through";
+    } else {
+        document.getElementById("newlist").style.color = "black";
+        document.getElementById("newlist").style.textDecoration = "none";
+    }
+}
 
-// const Delete = () => {
-//     let ul = (document.querySelector("ul").innerHTML = "");
-// };
+function DeleteOne() {}
