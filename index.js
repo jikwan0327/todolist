76b4c7f10ec72f1f6ba8f6dbeb6bdd929a6b25c8
jikwan <jikwan0327@gmail.com>
2022-05-22 23:42:11 +0900

    todolist
    (6)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var toDoList = Array();
 var doToList = "";
 var i = 0;
+var checknum = Array(0);
+document.querySelector(".left-item").innerHTML = toDoList.length + "items left";
+
 function getInput() {
     toDoList.push(document.getElementById("addlist").value);
-    document.querySelector("ul").innerHTML = "";
+    document.querySelector(".left-item").innerHTML = "";
     getInputInput();
 }
 
@@ -11,16 +14,29 @@ function getInputInput() {
     doToList = "";
     i = 0;
     for (var k = 0; k < toDoList.length; k++) {
-        doToList +=
-            "<li>" +
-            `<button class="delOne" onclick="DeleteOne(${k})">X</button>` +
-            `<input type="checkbox" class="checkList" onclick="is_checked(${k})"/>` +
-            '<div class="newlist">' +
-            toDoList[i + k] +
-            "</div>" +
-            "</li>";
+        console.log(checknum[k]);
+        if (checknum[k] === 1) {
+            doToList +=
+                "<li>" +
+                `<button class="delOne" onclick="DeleteOne(${k})">X</button>` +
+                `<input type="checkbox" class="checkList" onclick="is_checked(${k}) checked="true" "/>` +
+                '<div class="newlist">' +
+                toDoList[i + k] +
+                "</div>" +
+                "</li>";
+        } else {
+            doToList +=
+                "<li>" +
+                `<button class="delOne" onclick="DeleteOne(${k})">X</button>` +
+                `<input type="checkbox" class="checkList" onclick="is_checked(${k})"/>` +
+                '<div class="newlist">' +
+                toDoList[i + k] +
+                "</div>" +
+                "</li>";
+        }
     }
     document.querySelector("ul").innerHTML = doToList;
+    document.querySelector(".left-item").innerHTML = toDoList.length + "items left";
 }
 
 // for (var i = 0; i < toDoList.length; i++) {
@@ -41,14 +57,19 @@ function is_checked(value) {
     if (is_checked) {
         checkName[value].style.color = "rgba(200, 200, 200, 100";
         checkName[value].style.textDecoration = "line-through";
+        checknum[value] = 1;
+        console.log(checknum[value]);
     } else {
         checkName[value].style.color = "black";
         checkName[value].style.textDecoration = "none";
+        checknum[value] = 0;
+        console.log(checknum[value]);
     }
 }
 
 function DeleteOne(value) {
     var oneDel = document.getElementsByClassName("delOne");
+    checknum.splice(value, 1);
     toDoList.splice(value, 1);
     getInputInput();
 }
