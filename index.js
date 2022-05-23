910555aaab7e31e773653e096064442653cdddf7
jikwan <jikwan0327@gmail.com>
2022-05-23 23:17:43 +0900

    todolist
    7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var toDoList = Array();
 var doToList = "";
 var i = 0;
-var checknum = Array(0);
+var checknum = Array();
 document.querySelector(".left-item").innerHTML = toDoList.length + "items left";
 
 function getInput() {
@@ -14,13 +14,12 @@ function getInputInput() {
     doToList = "";
     i = 0;
     for (var k = 0; k < toDoList.length; k++) {
-        console.log(checknum[k]);
         if (checknum[k] === 1) {
             doToList +=
                 "<li>" +
                 `<button class="delOne" onclick="DeleteOne(${k})">X</button>` +
-                `<input type="checkbox" class="checkList" onclick="is_checked(${k}) checked="true" "/>` +
-                '<div class="newlist">' +
+                `<input type="checkbox" class="checkList" onclick="is_checked(${k})" checked="true"/>` +
+                '<div class="newlist" style="color: rgba(200, 200, 200, 100);text-decoration:line-through;" >' +
                 toDoList[i + k] +
                 "</div>" +
                 "</li>";
@@ -58,12 +57,10 @@ function is_checked(value) {
         checkName[value].style.color = "rgba(200, 200, 200, 100";
         checkName[value].style.textDecoration = "line-through";
         checknum[value] = 1;
-        console.log(checknum[value]);
     } else {
         checkName[value].style.color = "black";
         checkName[value].style.textDecoration = "none";
         checknum[value] = 0;
-        console.log(checknum[value]);
     }
 }
 
@@ -81,3 +78,29 @@ input.addEventListener("keypress", function (event) {
         document.getElementById("reset").click();
     }
 });
+
+const active = (value) => {
+    var activeSelect = document.getElementsByClassName("active");
+    if (value === 1) {
+        activeSelect[0].style.borderColor = "lightcoral";
+        activeSelect[1].style.borderColor = "black";
+        activeSelect[2].style.borderColor = "black";
+    }
+    if (value === 2) {
+        activeSelect[0].style.borderColor = "black";
+        activeSelect[1].style.borderColor = "lightcoral";
+        activeSelect[2].style.borderColor = "black";
+        for (var i = 0; i < toDoList.length; i++) {
+            if (checknum[i] === 1) {
+                checknum.splice(i, 1);
+                toDoList.splice(i, 1);
+                getInputInput();
+            }
+        }
+    }
+    if (value === 3) {
+        activeSelect[0].style.borderColor = "black";
+        activeSelect[1].style.borderColor = "black";
+        activeSelect[2].style.borderColor = "lightcoral";
+    }
+};
